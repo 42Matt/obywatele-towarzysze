5b9e651d8ae60bce847bf1c99a1bdbaa0ce61203
42Matt <45406097+42Matt@users.noreply.github.com>
2021-03-30 01:12:37 +0200

    Style: add fonts, theme gradients, happyRems
    fonts: Courgette, EmblemaOne,
    theme: sizes, fonts, gradiens,
    add happy rems and font antialiasing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,42 @@ import theme from 'theme/globalTheme';
 
 const GlobalStyle = createGlobalStyle`
   ${ModernNormalize}
+
+  *, *::before, *::after {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+  html {
+  // * Happy rems-> 1 rem === 10px
+  font-size: 62.5%;
+  }
+
+  @font-face {
+        font-family: 'Courgette';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: local('Courgette'), url('/fonts/Courgette-Regular.ttf') format('truetype');
+  }
+  @font-face {
+        font-family: 'EmblemaOne';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: local('EmblemaOne'), url('/fonts/EmblemaOne-Regular.ttf') format('truetype');
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    font-family: 'Courgette',-apple-system, BlinkMacSystemFont, 'Segoe UI',  'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
   }
+
 `;
 
 // eslint-disable-next-line react/prop-types
--- src/theme/globalTheme.js
+++ src/theme/globalTheme.js
@@ -8,6 +8,22 @@ const theme = {
     textNormal: '#111111',
     textWhite: '#FFF',
   },
+  gradients: {
+    primaryGradient: 'linear-gradient(94.56deg, #F9B5A8 12.48%, #ED6A40 82.12%)',
+    secondaryGradient: 'linear-gradient(94.56deg, #758086 13.57%, #2C4349 82.12%)',
+  },
+  sizes: {
+    xxl: '3.2rem',
+    xl: '2.4rem',
+    l: '1.8rem',
+    m: '1.6rem',
+    s: '1.4rem',
+    xs: '1.2rem',
+  },
+  fonts: {
+    title: 'EmblemaOne',
+    text: 'Courgette',
+  },
 };
 
 export default theme;
