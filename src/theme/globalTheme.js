9b1cc57c88d350983e7f24dd7c87c3453d70881d
Maciej Michalski <maciejmichalski92@gmail.com>
2021-03-29 22:41:57 +0200

    Fix: remove husky, lint-staged. Win10 err

--- src/theme/globalTheme.js
+++ src/theme/globalTheme.js
@@ -1,6 +1,12 @@
 const theme = {
   colors: {
-    primary: '#0090f3',
+    primary: '#FCB7A8',
+    primaryBackground: '#ED6A40',
+    accent: '#758086',
+    accentBackground: ' #2B4448',
+    background: '#FDD8CF',
+    textNormal: '#111111',
+    textWhite: '#FFF',
   },
 };
 
